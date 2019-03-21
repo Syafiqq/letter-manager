020f78c41309b60db1512d21ad0e76478e77adee
Syafiq <syafiq.rezpector@gmail.com>
2019-03-21 22:37:50 +0700

    Add custom css bundle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'letter-manager';
+
+  ngOnInit() {
+    const head = document.getElementsByTagName('head')[0];
+    const link = document.createElement('link');
+    link.id = 'cssId';
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'assets/themes/material.css';
+    link.media = 'all';
+    head.appendChild(link);
+  }
 }
