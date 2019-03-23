16c0fff7bcc9ccef7b085a222679c1304f7b1f2a
Syafiq <syafiq.rezpector@gmail.com>
2019-03-23 11:51:12 +0700

    Add buttom module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,11 +3,12 @@ import {NgModule} from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
-  imports: [MatGridListModule, MatCardModule, MatToolbarModule],
-  exports: [MatGridListModule, MatCardModule, MatToolbarModule],
+  imports: [MatGridListModule, MatCardModule, MatToolbarModule, MatButtonModule],
+  exports: [MatGridListModule, MatCardModule, MatToolbarModule, MatButtonModule],
 
 })
 
