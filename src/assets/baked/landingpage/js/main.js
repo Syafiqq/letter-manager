7c0ff29018667bd7536e5609e4ff0633bd1f7626
Syafiq <syafiq.rezpector@gmail.com>
2019-03-23 07:49:36 +0700

    Remove mailchimp

--- src/assets/baked/landingpage/js/main.js
+++ src/assets/baked/landingpage/js/main.js
@@ -1,16 +1,16 @@
 $(function() {
   "use strict";
 
-  var nav_offset_top = $('header').height() + 50; 
+  var nav_offset_top = $('header').height() + 50;
     /*-------------------------------------------------------------------------------
-	  Navbar 
+	  Navbar
 	-------------------------------------------------------------------------------*/
 
-	//* Navbar Fixed  
+	//* Navbar Fixed
     function navbarFixed(){
-        if ( $('.header_area').length ){ 
+        if ( $('.header_area').length ){
             $(window).scroll(function() {
-                var scroll = $(window).scrollTop();   
+                var scroll = $(window).scrollTop();
                 if (scroll >= nav_offset_top ) {
                     $(".header_area").addClass("navbar_fixed");
                 } else {
@@ -23,13 +23,6 @@ $(function() {
 
 
 
-
-
-  //------- mailchimp --------//  
-	function mailChimp() {
-		$('#mc_embed_signup').find('form').ajaxChimp();
-	}
-  mailChimp();
 
 
   /*-------------------------------------------------------------------------------
@@ -107,7 +100,7 @@ $(function() {
 
 
 
-  
+
 });
 
 
