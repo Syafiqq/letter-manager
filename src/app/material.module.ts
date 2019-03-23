325b76cf0d67974f86449efccaaddfd5efd89b25
Syafiq <syafiq.rezpector@gmail.com>
2019-03-23 13:05:06 +0700

    Add input module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,12 +4,12 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
-  imports: [MatGridListModule, MatCardModule, MatToolbarModule, MatButtonModule],
-  exports: [MatGridListModule, MatCardModule, MatToolbarModule, MatButtonModule],
-
+  imports: [MatGridListModule, MatCardModule, MatToolbarModule, MatButtonModule, MatInputModule],
+  exports: [MatGridListModule, MatCardModule, MatToolbarModule, MatButtonModule, MatInputModule],
 })
 
 export class MaterialModule {
