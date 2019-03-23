f84cab9d96d6c22c0e4f07b4d70f76996d6018b2
Syafiq <syafiq.rezpector@gmail.com>
2019-03-23 17:19:12 +0700

    register register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import {RouterModule, Routes} from '@angular/router';
 import {LandingPageComponent} from './landing-page/landing-page.component';
 import {AppComponent} from './app.component';
 import {LoginPageComponent} from './login-page/login-page.component';
+import {RegisterPageComponent} from './register-page/register-page.component';
 
 const routes: Routes = [
   {path: 'landingpage', component: LandingPageComponent},
   {path: 'loginpage', component: LoginPageComponent},
+  {path: 'registerpage', component: RegisterPageComponent},
   {path: '', component: AppComponent},
 ];
 
