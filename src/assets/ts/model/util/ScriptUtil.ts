d0863b60df402577009b8cd6b56b8c62727babce
Syafiq <syafiq.rezpector@gmail.com>
2019-03-23 07:42:10 +0700

    Remove ID

--- src/assets/ts/model/util/ScriptUtil.ts
+++ src/assets/ts/model/util/ScriptUtil.ts
@@ -1,8 +1,7 @@
 export class ScriptUtil {
-  public static scriptJsInjector(id: string, document: Document, path: string) {
+  public static scriptJsInjector(document: Document, path: string) {
     const head = document.getElementsByTagName('body')[0];
     const script = document.createElement('script');
-    script.id = id;
     script.type = 'text/javascript';
     script.src = path;
     head.appendChild(script);
--- src/assets/ts/model/util/StyleUtil.ts
+++ src/assets/ts/model/util/StyleUtil.ts
@@ -1,8 +1,7 @@
 export class StyleUtil {
-  public static styleCssInjector(id: string, document: Document, path: string) {
+  public static styleCssInjector(document: Document, path: string) {
     const head = document.getElementsByTagName('head')[0];
     const link = document.createElement('link');
-    link.id = id;
     link.rel = 'stylesheet';
     link.type = 'text/css';
     link.href = path;
