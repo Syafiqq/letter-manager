5e40b797dd6e6330d78461498b6a42e83927922b
Syafiq <syafiq.rezpector@gmail.com>
2019-03-23 08:00:23 +0700

    Register loginpage router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LandingPageComponent} from './landing-page/landing-page.component';
 import {AppComponent} from './app.component';
+import {LoginPageComponent} from './login-page/login-page.component';
 
 const routes: Routes = [
   {path: 'landingpage', component: LandingPageComponent},
+  {path: 'loginpage', component: LoginPageComponent},
   {path: '', component: AppComponent},
 ];
 
