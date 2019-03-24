114131c8895654948966f15822bcae078fd81f92
Syafiq <syafiq.rezpector@gmail.com>
2019-03-24 13:07:08 +0700

    Register archive page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import {AppComponent} from './app.component';
 import {LoginPageComponent} from './login-page/login-page.component';
 import {RegisterPageComponent} from './register-page/register-page.component';
 import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
+import {ArchivePageComponent} from './archive-page/archive-page.component';
 
 const routes: Routes = [
   {path: 'landingpage', component: LandingPageComponent},
   {path: 'loginpage', component: LoginPageComponent},
   {path: 'registerpage', component: RegisterPageComponent},
   {path: 'dashboardpage', component: DashboardPageComponent},
+  {path: 'archivepage', component: ArchivePageComponent},
   {path: '', component: AppComponent},
 ];
 
