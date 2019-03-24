b8a4fdcac3b3da21542ce8e42fd2417bf472d1f4
Syafiq <syafiq.rezpector@gmail.com>
2019-03-24 16:17:16 +0700

    Add initial theme

--- src/app/detail-page/detail-page.component.ts
+++ src/app/detail-page/detail-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {StyleUtil} from '../../assets/ts/model/util/StyleUtil';
 
 @Component({
   selector: 'app-detail-page',
@@ -11,6 +12,9 @@ export class DetailPageComponent implements OnInit {
   }
 
   ngOnInit() {
+    StyleUtil.styleCssInjector(document, 'assets/themes/bulma.css');
+    StyleUtil.styleCssInjector(document, 'assets/themes/material.css');
+    document.body.classList.add('has-background-white-ter');
   }
 
 }
