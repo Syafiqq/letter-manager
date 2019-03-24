7a6bf50c03de1b2b757642a3e51d173f42c8a4fd
Syafiq <syafiq.rezpector@gmail.com>
2019-03-24 18:16:47 +0700

    Register mat stepper

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -14,6 +14,7 @@ import {MatTableModule} from '@angular/material/table';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
+import {MatStepperModule} from '@angular/material/stepper';
 
 
 @NgModule({
@@ -31,7 +32,8 @@ import {MatListModule} from '@angular/material/list';
     MatTableModule,
     MatPaginatorModule,
     MatDividerModule,
-    MatListModule
+    MatListModule,
+    MatStepperModule
   ],
   exports: [
     MatGridListModule,
@@ -47,7 +49,8 @@ import {MatListModule} from '@angular/material/list';
     MatTableModule,
     MatPaginatorModule,
     MatDividerModule,
-    MatListModule
+    MatListModule,
+    MatStepperModule
   ], providers: [
     MatDatepickerModule,
   ],
