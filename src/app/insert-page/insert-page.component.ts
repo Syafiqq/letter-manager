30dc5e0904bc84df6db04611b0122492f6fd6b96
Syafiq <syafiq.rezpector@gmail.com>
2019-03-24 18:57:20 +0700

    Finishing insert page

--- src/app/insert-page/insert-page.component.ts
+++ src/app/insert-page/insert-page.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {StyleUtil} from '../../assets/ts/model/util/StyleUtil';
+import * as PDFObject from '../../../node_modules/pdfobject';
 
 @Component({
   selector: 'app-insert-page',
@@ -6,11 +8,20 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./insert-page.component.scss']
 })
 export class InsertPageComponent implements OnInit {
+  support = false;
 
   constructor() {
   }
 
   ngOnInit() {
+    StyleUtil.styleCssInjector(document, 'assets/themes/bulma.css');
+    StyleUtil.styleCssInjector(document, 'assets/themes/material.css');
+    document.body.classList.add('has-background-white-ter');
+    this.support = PDFObject.supportsPDFs;
+    /*if (this.support) {
+      const container = document.querySelector('#my-pdf');
+      PDFObject.embed('https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf', container);
+    }*/
   }
 
 }
