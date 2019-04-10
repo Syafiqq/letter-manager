08589be878cdaa2fcbf4b6d0053754946a8cc42d
Syafiq <syafiq.rezpector@gmail.com>
2019-04-10 22:15:03 +0700

    Add http client and env service

--- src/app/services/http/auth.service.ts
+++ src/app/services/http/auth.service.ts
@@ -1,10 +1,13 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {EnvService} from '../config/env.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() {
+  constructor(private http: HttpClient, private env: EnvService) {
   }
 }
