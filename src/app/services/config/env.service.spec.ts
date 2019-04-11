f0d0d73b3dbd1bfcb0894a8f6c35f56271b4e6f1
Syafiq <syafiq.rezpector@gmail.com>
2019-04-11 22:29:18 +0700

    Test EnvService with provider

--- src/app/services/config/env.service.spec.ts
+++ src/app/services/config/env.service.spec.ts
@@ -1,9 +1,19 @@
 import {TestBed} from '@angular/core/testing';
 
 import {EnvService} from './env.service';
+import {EnvServiceProvider} from './env.service.provider';
 
 describe('EnvService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let globEnvService: EnvService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        EnvServiceProvider
+      ]
+    });
+    globEnvService = TestBed.get(EnvService);
+  });
 
   it('should be created', () => {
     const service: EnvService = TestBed.get(EnvService);
@@ -14,5 +24,10 @@ describe('EnvService', () => {
     const service: EnvService = TestBed.get(EnvService);
     service.apiUrl = 'new url';
     expect(service.apiUrl).toEqual('new url');
+  });
+
+  it('should create same object', () => {
+    const service: EnvService = TestBed.get(EnvService);
+    expect(globEnvService).toEqual(service);
   });
 });
