535dc46874f2d9307686738cdb41c20a5bed486b
Syafiq <syafiq.rezpector@gmail.com>
2019-04-11 22:03:21 +0700

    Add api url

--- src/app/services/http/auth.service.ts
+++ src/app/services/http/auth.service.ts
@@ -7,7 +7,16 @@ import {EnvService} from '../config/env.service';
   providedIn: 'root'
 })
 export class AuthService {
+  public baseUrl = this.env.apiUrl;
 
   constructor(private http: HttpClient, private env: EnvService) {
+  }
+
+  getVersion() {
+    return this.http.get(this.baseUrl);
+  }
+
+  getUrl() {
+    return this.env.apiUrl;
   }
 }
